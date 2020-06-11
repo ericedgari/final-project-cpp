661506beda46cbf9dc86217518ecffa7fa37449f
dankpanda <yowenyo17@gmail.com>
2020-06-11 17:00:13 +0700

    added comments

--- GUI/avl.h
+++ GUI/avl.h
@@ -5,6 +5,7 @@ using namespace std;
 
 class AVLTree{
     
+    // Structure of a leaf node
     struct Node{
         int number;
         string name;
@@ -15,17 +16,21 @@ class AVLTree{
     
 private:
 Node* root;
+vector<int> idList;
+vector<string> nameList;
 
-
-int getHeight(Node *node){
+// Returns the height of a specified leaf node
+int getHeight(Node *node){ 
     if(node == NULL) return 0;
     return node -> height;
 }
 
+// Returns the balance factor of a specified leaf node
 int getBalance(Node *node){
     return getHeight(node -> left) - getHeight(node -> right);
 }
 
+// A function to create a new node
 Node* createNode(int number, string name){
     Node* temp = new Node();
     temp -> number = number;
@@ -36,10 +41,12 @@ Node* createNode(int number, string name){
     return temp;
 }
 
+// A function to update the height of a node after changes
 void updateHeight(Node *node){
     node -> height = max(getHeight(node -> left), getHeight(node -> right)) + 1;
 }
 
+// A function to right rotate the tree using a specified node as a pivot
 Node* rightRotate(Node *node){
     Node *temp1 = node -> left;
     Node *temp2 = temp1 -> right;
@@ -47,11 +54,13 @@ Node* rightRotate(Node *node){
     temp1 -> right = node;
     node -> left = temp2;
     
-    updateHeight(node);
+    // Updates the height of the nodes involved to check whether further rotations are required
+    updateHeight(node); 
     updateHeight(temp1);
     return temp1;
 }
 
+// A function to left rotate the tree using a specified node as a pivot
 Node* leftRotate(Node *node){
     Node *temp1 = node -> right;
     Node *temp2 = temp1 -> left;
@@ -59,18 +68,21 @@ Node* leftRotate(Node *node){
     temp1 -> left = node;
     node -> right = temp2;
     
+    // Updates the height of the nodes involved to check whether further rotations are required
     updateHeight(node);
     updateHeight(temp1);
     return temp1;
 }
 
+// Function to insert a new leaf to the tree
 Node* insertP(Node* node, int number, string name){
-    if(node == NULL){
-        node = createNode(number, name);
+    if(node == NULL){   
+        node = createNode(number, name); // This checks whether the root is empty or not
     }
     
+    // These 2 if statements makes the function run recursively to determine the appropriate location to allocate the new node
     else if(node -> number < number){
-        node -> right = insertP(node -> right, number, name);
+        node -> right = insertP(node -> right, number, name); 
     }
     
     else if(node -> number > number){
@@ -79,10 +91,13 @@ Node* insertP(Node* node, int number, string name){
     
     else return node;
     
+    // Updates the height of the new node
     updateHeight(node);
     
+    // Gets the balance factor of the new node
     int balance = getBalance(node);
-
+    
+    // The following 4 if statements checks whether the subtree is on a left-left case, left-right case, right-right case or right-left case
     if(balance > 1 && number < node -> left -> number){
         node = rightRotate(node);
     }
@@ -105,6 +120,7 @@ Node* insertP(Node* node, int number, string name){
     return node;
 }
 
+// Prints the tree in preorder
 void printPreOrderP(Node* node){
     if(node != NULL){
         cout << "Name: " << node -> name  << "\t" << "Number: " << node -> number << endl;
@@ -115,7 +131,7 @@ void printPreOrderP(Node* node){
 }
 
 
-
+// Update vector for use in GUI in order of ID
 void updateVectorInOrderP(Node* node){
     if(node != NULL){
         updateVectorInOrderP(node -> left);
@@ -126,13 +142,14 @@ void updateVectorInOrderP(Node* node){
 }
 
 
-
+// Helper function to obtain minimum value of a subtree
 Node* getMin(Node* node){
     if(root == NULL) return root;
     else if(node -> left == NULL) return node;
     else getMin(node -> left);
 }
 
+// Function to remove a node from the tree
 Node* removeP(Node* node, int number){
     if(root == NULL){
         return root;
@@ -142,6 +159,7 @@ Node* removeP(Node* node, int number){
         return node;
     }
     
+    // The following 2 if function makes this function run recursively to the designated node
     else if(number > node -> number){
         node -> right = removeP(node -> right, number);
     }
@@ -151,6 +169,7 @@ Node* removeP(Node* node, int number){
     }
     
     else{
+        // Case of deleted node having 2 childs
         if(node -> left && node -> right){
             Node* temp = getMin(node -> right); 
             node -> number = temp -> number;
@@ -158,6 +177,7 @@ Node* removeP(Node* node, int number){
         }
         
         else{
+            // Case of deleted node having 1 or 0 child
             Node* temp = node -> left? node -> left : node -> right;
             if(temp == NULL){
                 temp = node;
@@ -173,10 +193,13 @@ Node* removeP(Node* node, int number){
         return node;
     }
     
+    // Updates the height of the new subtree after deletion
     updateHeight(node);
     
+    // Obtains balance factor
     int balance = getBalance(node);
     
+     // The following 4 if statements checks whether the subtree is on a left-left case, left-right case, right-right case or right-left case
     if(balance > 1 && number < node -> left -> number){
         node = rightRotate(node);
     }
@@ -199,6 +222,7 @@ Node* removeP(Node* node, int number){
     return node;
 }
 
+// Function to search for a specified id
 Node* searchP(Node* node, int number){
     if(node == NULL){
         return node;
@@ -220,9 +244,8 @@ public:
 AVLTree(){
     root = NULL;
 }
-vector<int> idList;
-vector<string> nameList;
 
+// Public insert to call private function insertP()
 void insert(int number, string name){
     root = insertP(root, number, name);
     nameList.clear();
@@ -230,35 +253,40 @@ void insert(int number, string name){
     updateVectorInOrder();
 }
 
+// Public updateVector to call private function updateVectorInOrder()
 void updateVector(){
     nameList.clear();
     idList.clear();
     updateVectorInOrder();
     }
 
+// Public printPreOrder to call private function printPreOrderP()
 void printPreOrder(){
     printPreOrderP(root);
 }
 
 
+// Public updateVectorInOrder to call private function updateVectorInOrderP()
 void updateVectorInOrder(){
     updateVectorInOrderP(root);
 }
 
-    
-
+// Function to return a vector containing all the names
 vector<string> getNameList(){
     return nameList;
     }
-    
+
+// Function to return a vector containing all the ids
 vector<int> getIdList(){
     return idList;
     }   
 
+// Public remove to call private function removeP()
 void remove(int number){
     root = removeP(root, number);
 }
 
+// Public search to call private function searchP()
 string search(int number){
     if(searchP(root, number) != NULL){
         return searchP(root, number) -> name;
@@ -266,12 +294,14 @@ string search(int number){
     else return "NAN";
 }
 
+// Function to print out items inside vector containing ints
 void printVector(vector<int> v){
     for (int i =0 ; i<v.size();i++){
         cout<<v[i]<<endl;
     }
 }
 
+// Function to print out items inside vector containing strings
 void printVector(vector<string> v){
         for (int i =0 ; i<v.size();i++){
             cout<<v[i]<<endl;
